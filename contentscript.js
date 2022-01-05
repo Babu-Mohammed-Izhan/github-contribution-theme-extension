5e9aa06dd3b81ca96a8990b261e2bcf4fbb07331
Izhan <babuizhan1234@gmail.com>
2022-01-05 19:20:26 +0530

    Added progress bar color support and changed color schemes

--- contentscript.js
+++ contentscript.js
@@ -1,27 +1,27 @@
 "use strict";
 
-var github = ["#161B22", "#9be9a8", "#40c463", "#30a14e", "#216e39"];
-var halloween = ["#161B22", "#fdf156", "#ffc722", "#ff9711", "#04001b"];
-var christmas = ["#161B22", "#eeeeee ", "#D03B3B", "#136637", "#28543C"];
+var github = [  "#9be9a8", "#40c463", "#30a14e", "#216e39","#161B22"];
+var halloween = [  "#fdf156", "#ffc722", "#ff9711", "#04001b","#161B22"];
+var christmas = [  "#eeeeee ", "#D03B3B", "#136637", "#28543C","#161B22"];
 
-var amber = ["#161B22", "#ffecb3", "#ffd54f", "#ffb300", "#ff6f00"];
-var blue = ["#161B22", "#bbdefb", "#64b5f6", "#1e88e5", "#0d47a1"];
-var bluegrey = ["#161B22", "#cfd8dc", "#90a4ae", "#546e7a", "#263238"];
-var brown = ["#161B22", "#d7ccc8", "#a1887f", "#6d4c41", "#3e2723"];
-var cyan = ["#161B22", "#b2ebf2", "#4dd0e1", "#00acc1", "#006064"];
-var deeporange = ["#161B22", "#ffccbc", "#ff8a65", "#f4511e", "#bf360c"];
-var deeppurple = ["#161B22", "#d1c4e9", "#9575cd", "#5e35b1", "#311b92"];
-var green = ["#161B22", "#c8e6c9", "#81c784", "#43a047", "#1b5e20"];
-var grey = ["#161B22", "#e0e0e0", "#9e9e9e", "#616161", "#212121"];
-var indigo = ["#161B22", "#c5cae9", "#7986cb", "#3949ab", "#1a237e"];
-var lightblue = ["#161B22", "#b3e5fc", "#4fc3f7", "#039be5", "#01579b"];
-var lightgreen = ["#161B22", "#dcedc8", "#aed581", "#7cb342", "#33691e"];
-var lime = ["#161B22", "#f0f4c3", "#dce775", "#c0ca33", "#827717"];
-var orange = ["#161B22", "#ffe0b2", "#ffb74d", "#fb8c00", "#e65100"];
-var pink = ["#161B22", "#f8bbd0", "#f06292", "#e91e63", "#880e4f"];
-var purple = ["#161B22", "#e1bee7", "#ba68c8", "#8e24aa", "#4a148c"];
-var red = ["#161B22", "#ffcdd2", "#e57373", "#e53935", "#b71c1c"];
-var teal = ["#161B22", "#b2dfdb", "#4db6ac", "#00897b", "#004d40"];
+var amber = [ "#ffecb3", "#ffd54f", "#ffb300", "#ff6f00","#161B22"];
+var blue = [  "#bbdefb", "#64b5f6", "#1e88e5", "#0d47a1","#161B22"];
+var bluegrey = [  "#cfd8dc", "#90a4ae", "#546e7a", "#263238","#161B22"];
+var brown = [  "#d7ccc8", "#a1887f", "#6d4c41", "#3e2723","#161B22"];
+var cyan = [  "#b2ebf2", "#4dd0e1", "#00acc1", "#006064","#161B22"];
+var deeporange = [  "#ffccbc", "#ff8a65", "#f4511e", "#bf360c","#161B22"];
+var deeppurple = [  "#d1c4e9", "#9575cd", "#5e35b1", "#311b92","#161B22"];
+var green = [  "#c8e6c9", "#81c784", "#43a047", "#1b5e20","#161B22"];
+var grey = [  "#e0e0e0", "#9e9e9e", "#616161", "#212121","#161B22"];
+var indigo = [  "#c5cae9", "#7986cb", "#3949ab", "#1a237e","#161B22"];
+var lightblue = [  "#b3e5fc", "#4fc3f7", "#039be5", "#01579b","#161B22"];
+var lightgreen = [  "#dcedc8", "#aed581", "#7cb342", "#33691e","#161B22"];
+var lime = [  "#f0f4c3", "#dce775", "#c0ca33", "#827717","#161B22"];
+var orange = [  "#ffe0b2", "#ffb74d", "#fb8c00", "#e65100","#161B22"];
+var pink = [  "#f8bbd0", "#f06292", "#e91e63", "#880e4f","#161B22"];
+var purple = [  "#e1bee7", "#ba68c8", "#8e24aa", "#4a148c","#161B22"];
+var red = [  "#ffcdd2", "#e57373", "#e53935", "#b71c1c","#161B22"];
+var teal = [  "#b2dfdb", "#4db6ac", "#00897b", "#004d40","#161B22"];
 
 var colors = {
   github: github,
@@ -69,7 +69,7 @@ function applyColorToLegend(color) {
     var lis = doc[0].getElementsByTagName("li");
 
     for (var i = 1, l = lis.length; i < l; i++) {
-      lis[i].style.setProperty("background-color", color[i], "important");
+      lis[i].style.setProperty("background-color", color[l-i], "important");
     }
   }
 }
@@ -98,14 +98,27 @@ function applyColorToActivity(color) {
   }
 }
 
+function applyColorToProgress(color) {
+  var progress = document.getElementsByClassName("Progress-item");
+  if (progress[0]) {
+
+    for (var i = 1, l = progress.length; i < l; i++) {
+      progress[i].style.setProperty("background-color", color[2], "important");
+    }
+  }
+  
+}
+
+
 function applyOptions() {
   chrome.storage.local.get("favoriteColor", function (color) {
     if (!color.favoriteColor) {
       color.favoriteColor = "halloween";
     }
-    applyColorToCssGitHubVars(colors[color.favoriteColor]);
-    applyColorToLegend(colors[color.favoriteColor]);
-    applyColorToActivity(colors[color.favoriteColor]);
+    applyColorToCssGitHubVars(colors[color.favoriteColor].reverse());
+    applyColorToLegend(colors[color.favoriteColor].reverse());
+    applyColorToProgress(colors[color.favoriteColor].reverse());
+    applyColorToActivity(colors[color.favoriteColor].reverse());
   });
 }
 
